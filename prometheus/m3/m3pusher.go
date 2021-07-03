f5ad0588fc4180b6d532018ecc0b32ef5430d942
wangjc <edy@wangjc-MacbookPro.local>
2021-07-03 13:19:41 +0800

    Update M3Pusher Method

--- prometheus/m3/m3pusher.go
+++ prometheus/m3/m3pusher.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
 	dto "github.com/prometheus/client_model/go"
@@ -35,7 +37,8 @@ type M3Pusher struct {
 	useBasicAuth       bool
 	username, password string
 
-	ctx context.Context
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
 //初始化选项，包括本地缓存空间设置
@@ -43,13 +46,22 @@ type M3PusherOpt func(*M3Pusher)
 
 func WithHttpPutOpt() M3PusherOpt {
 	return func(p *M3Pusher) {
-		p.httpMethod = "PUT"
+		p.httpMethod = http.MethodPut
 	}
 }
 
 func WithHttpPostOpt() M3PusherOpt {
 	return func(p *M3Pusher) {
-		p.httpMethod = "POST"
+		p.httpMethod = http.MethodPost
+	}
+}
+
+//DefaultRegister在初始化的时候就采集了process和go相关指标
+func WithDefaultRegistererOpt() M3PusherOpt {
+	return func(p *M3Pusher) {
+		p.registerer = prometheus.DefaultRegisterer
+		//p.gatherers = prometheus.Gatherers{prometheus.DefaultGatherer.(*prometheus.Registry)}
+		p.gatherers = prometheus.Gatherers{prometheus.DefaultGatherer}
 	}
 }
 
@@ -61,11 +73,19 @@ func New(url, namespace string, opts ...M3PusherOpt) *M3Pusher {
 		err error
 	)
 
+	if !strings.Contains(url, "://") {
+		url = "http://" + url
+	}
+	if strings.HasSuffix(url, "/") {
+		url = url[:len(url)-1]
+	}
+
 	p := &M3Pusher{
 		url:        url,
 		namespace:  namespace,
 		error:      err,
 		registerer: reg,
+		httpMethod: http.MethodPost,
 		grouping:   map[string]string{},
 		gatherers:  prometheus.Gatherers{reg},
 		client:     &http.Client{},
@@ -78,9 +98,10 @@ func New(url, namespace string, opts ...M3PusherOpt) *M3Pusher {
 
 //配置HTTP Basic Authentication
 func (p *M3Pusher) BasicAuth(username, password string) *M3Pusher {
-	p.useBasicAuth = true
-	p.username = username
-	p.password = password
+	//todo 尚未使用到，需对M3进行设置后再使用
+	//p.useBasicAuth = true
+	//p.username = username
+	//p.password = password
 	return p
 }
 
@@ -104,14 +125,12 @@ func (p *M3Pusher) Gatherer(g prometheus.Gatherer) *M3Pusher {
 	return p
 }
 
-//将数据推送到M3后清空本地缓存
-//func (p *M3Pusher) PushWithEmpty() error { return nil }
-
 //将数据推送到M3后仍把数据保存在本地
 func (p *M3Pusher) Push() error {
 	return p.push()
 }
 
+//todo  如何对该接口进行并发测试
 func (p *M3Pusher) push() error {
 	if p.error != nil {
 		return p.error
@@ -120,18 +139,17 @@ func (p *M3Pusher) push() error {
 	if err != nil {
 		return err
 	}
-	timeSeries, err := translateMetricFamilytoWriteRequest(mfs)
+	timeSeries, err := transMFtoTS(mfs)
 	if err != nil {
 		return fmt.Errorf("translateMetricFamilytoWriteRequest error: %v", err)
 	}
 	buf, _, err := buildWriteRequest(timeSeries, nil, nil)
 	if err != nil {
-		fmt.Printf("buildWriteRequest error: %+v\n", err)
+		return fmt.Errorf("buildWriteRequest error: %+v\n", err)
 	}
-	//todo 对url需要进行检查并重新构造
 	req, err := http.NewRequest(p.httpMethod, fmt.Sprintf("%s/api/v1/prom/remote/write", p.url), bytes.NewReader(buf))
 	if err != nil {
-		fmt.Printf("http NewRequest error: %v\n", err)
+		return fmt.Errorf("http NewRequest error: %v\n", err)
 	}
 	if p.useBasicAuth {
 		req.SetBasicAuth(p.username, p.password)
@@ -148,12 +166,14 @@ func (p *M3Pusher) push() error {
 	return nil
 }
 
-func translateMetricFamilytoWriteRequest(mfs []*dto.MetricFamily) ([]prompb.TimeSeries, error) {
+//将MetricFamily格式转换为TimeSeries
+func transMFtoTS(mfs []*dto.MetricFamily) ([]prompb.TimeSeries, error) {
 	timeSeries := []prompb.TimeSeries{}
 	for _, mf := range mfs {
 		name := mf.GetName()
-		value := getValueFunc(mf)
-		//todo 需要确认如何将help提示直接推送到remote storage中
+		//value := getValueFunc(mf)
+		ts := returnTimeSeriesFunc(mf)
+		//todo 需要确认如何将help提示直接推送到remote storage中,可以抓包结合源码对 push 到pgw进行分析
 		//help := mf.GetHelp()
 		for _, m := range mf.Metric {
 			labels := []prompb.Label{}
@@ -165,10 +185,12 @@ func translateMetricFamilytoWriteRequest(mfs []*dto.MetricFamily) ([]prompb.Time
 					Name: label.GetName(), Value: label.GetValue(),
 				})
 			}
-			timeSeries = append(timeSeries, prompb.TimeSeries{
-				Labels:  labels,
-				Samples: []prompb.Sample{{Value: value(m), Timestamp: time.Now().UnixNano() / 1e6}},
-			})
+			//timeSeries = append(timeSeries, prompb.TimeSeries{
+			//	Labels:  labels,
+			//	Samples: []prompb.Sample{{Value: value(m), Timestamp: time.Now().UnixNano() / 1e6}},
+			//})
+			timeSeries = append(timeSeries, ts(labels, m, time.Now().UnixNano()/1e6)...)
+
 		}
 	}
 	return timeSeries, nil
@@ -176,57 +198,161 @@ func translateMetricFamilytoWriteRequest(mfs []*dto.MetricFamily) ([]prompb.Time
 
 type getValue func(metric *dto.Metric) float64
 
-func getValueFunc(mf *dto.MetricFamily) getValue {
+type getTimeSeries func(labels []prompb.Label, metric *dto.Metric, timestamp int64) []prompb.TimeSeries
+
+func returnTimeSeriesFunc(mf *dto.MetricFamily) getTimeSeries {
 	switch *mf.Type {
-	case dto.MetricType_COUNTER:
-		return func(metric *dto.Metric) float64 {
-			return *metric.Counter.Value
-		}
 	case dto.MetricType_GAUGE:
-		return func(metric *dto.Metric) float64 {
-			return *metric.Gauge.Value
+		return func(labels []prompb.Label, metric *dto.Metric, timestamp int64) []prompb.TimeSeries {
+			gauge := metric.GetGauge()
+			if gauge == nil {
+				return []prompb.TimeSeries{}
+			}
+			return []prompb.TimeSeries{{
+				Labels:  labels,
+				Samples: []prompb.Sample{{Value: gauge.GetValue(), Timestamp: timestamp}},
+			}}
+
 		}
-	//todo  当前只支持对Gauge和Counter的指标写入
-	//case dto.MetricType_SUMMARY:
-	//	getValue = func(metric *dto.Metric) float64 {
-	//		return *metric.Summary.SampleSum
-	//	}
-	//case dto.MetricType_UNTYPED:
-	//	getValue = func(metric *dto.Metric) float64 {
-	//		return *metric.Untyped.Value
-	//	}
-	//case dto.MetricType_HISTOGRAM:
-	//	getValue = func(metric *dto.Metric) float64 {
-	//		return *metric.Histogram.SampleSum
-	//	}
+	case dto.MetricType_COUNTER:
+		return func(labels []prompb.Label, metric *dto.Metric, timestamp int64) []prompb.TimeSeries {
+			counter := metric.GetCounter()
+			if counter == nil {
+				return []prompb.TimeSeries{}
+			}
+			return []prompb.TimeSeries{{
+				Labels:  labels,
+				Samples: []prompb.Sample{{Value: counter.GetValue(), Timestamp: timestamp}},
+			}}
+
+		}
+	case dto.MetricType_SUMMARY:
+		return func(labels []prompb.Label, metric *dto.Metric, timestamp int64) []prompb.TimeSeries {
+			summary := metric.GetSummary()
+			if summary == nil {
+				return []prompb.TimeSeries{}
+			}
+			ts := make([]prompb.TimeSeries, 0)
+
+			nameLabel := labels[0]
+			//_sum
+			ts = append(ts, prompb.TimeSeries{
+				Labels: append([]prompb.Label{{
+					Name: "__name__", Value: fmt.Sprintf("%s_sum", nameLabel.Value),
+				}}, labels[1:]...),
+				Samples: []prompb.Sample{
+					{Value: summary.GetSampleSum(), Timestamp: timestamp},
+				},
+			})
+			//_count
+			ts = append(ts, prompb.TimeSeries{
+				Labels: append([]prompb.Label{{
+					Name: "__name__", Value: fmt.Sprintf("%s_count", nameLabel.Value),
+				}}, labels[1:]...),
+				Samples: []prompb.Sample{
+					{Value: float64(summary.GetSampleCount()), Timestamp: timestamp},
+				},
+			})
+			//quantile metric
+			for _, quantile := range summary.GetQuantile() {
+				ts = append(ts, prompb.TimeSeries{
+					Labels: append(labels, prompb.Label{
+						Name:  "quantile",
+						Value: strconv.FormatFloat(quantile.GetQuantile(), 'f', 2, 64),
+					}),
+					Samples: []prompb.Sample{
+						{Value: quantile.GetValue(), Timestamp: timestamp},
+					},
+				})
+			}
+			return ts
+		}
+	case dto.MetricType_HISTOGRAM:
+		return func(labels []prompb.Label, metric *dto.Metric, timestamp int64) []prompb.TimeSeries {
+			histogram := metric.GetHistogram()
+			if histogram == nil {
+				return []prompb.TimeSeries{}
+			}
+			ts := make([]prompb.TimeSeries, 0)
+			ts = append(ts, prompb.TimeSeries{
+				Labels: labels,
+				Samples: []prompb.Sample{
+					{Value: histogram.GetSampleSum(), Timestamp: timestamp},
+					{Value: float64(histogram.GetSampleCount()), Timestamp: timestamp},
+				},
+			})
+			//sum
+			ts = append(ts, prompb.TimeSeries{
+				Labels: append([]prompb.Label{{
+					Name: "__name__", Value: fmt.Sprintf("%s_sum", labels[0].Value),
+				}}, labels[1:]...),
+				Samples: []prompb.Sample{
+					{Value: histogram.GetSampleSum(), Timestamp: timestamp},
+					//{Value: float64(histogram.GetSampleCount()), Timestamp: timestamp},
+				},
+			})
+			//count
+			ts = append(ts, prompb.TimeSeries{
+				Labels: append([]prompb.Label{{
+					Name: "__name__", Value: fmt.Sprintf("%s__count", labels[0].Value),
+				}}, labels[1:]...),
+				Samples: []prompb.Sample{
+					{Value: float64(histogram.GetSampleCount()), Timestamp: timestamp},
+				},
+			})
+			//le
+			for _, bucket := range histogram.GetBucket() {
+				ts = append(ts, prompb.TimeSeries{
+					Labels: append(labels, prompb.Label{
+						Name:  "le",
+						Value: strconv.FormatFloat(bucket.GetUpperBound(), 'f', 2, 64),
+					}),
+					Samples: []prompb.Sample{
+						{Value: float64(bucket.GetCumulativeCount()), Timestamp: timestamp},
+					},
+				})
+			}
+			return ts
+		}
 	default:
-		return func(*dto.Metric) float64 {
-			return 0
+		return func(labels []prompb.Label, metric *dto.Metric, timestamp int64) []prompb.TimeSeries {
+			return []prompb.TimeSeries{}
 		}
 	}
-
 }
 
 //根据当前Collector和Gatherer定时推送数据，推送后数据仍保存在本地
-//todo 需要对goroutine做管理，使用context做管理，可以参考毛大的Context
-func (p *M3Pusher) PushTimed(t time.Duration) {
-	p.ctx = context.Background()
-
-	timeTicker := time.NewTicker(t)
-	select {
-	case <-timeTicker.C:
-		p.push()
+//需要考虑并发的问题，如果并发，会有怎么样的影响。
+func PushTimed(p *M3Pusher, t time.Duration) error {
+	if p.cancel != nil {
+		p.cancel()
 	}
+	ctx, cancel := context.WithCancel(context.Background())
+	ctx = context.WithValue(ctx, "timeDuration", t)
+	p.cancel = cancel
+	//go p.pushWithContext(ctx)
 
-}
-func Go(f func()) {
-	go f()
+	go func() {
+		ticker := time.NewTicker(t)
+
+		for {
+			select {
+			case <-ctx.Done():
+				break
+			case <-ticker.C:
+				p.push()
+			}
+		}
+	}()
+	return nil
 }
 
 //对定时任务做取消
-func (p *M3Pusher) PushCanceld() {
-
+func PushCanceld(p *M3Pusher) {
+	if p.cancel == nil {
+		return
+	}
+	p.cancel()
+	p.cancel = nil
+	return
 }
-
-//根据当前Collector和Gatherer定时推送数据，推送后数据清空
-func (p *M3Pusher) PushTimedWithEmpty(t *time.Duration) error { return nil }
